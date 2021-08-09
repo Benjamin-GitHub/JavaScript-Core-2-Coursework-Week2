1f243460b11e6bf83ba6763043b8309ed79eccf7
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-08-09 23:23:27 +0100

    Exercise B
    Completed

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,15 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfShopping) {
+  let content = document.querySelector("#content");
+  let ul = document.createElement("ul");
+
+  arrayOfShopping.forEach((perItem) => {
+    let li = document.createElement("li");
+
+    content.appendChild(ul);
+    ul.appendChild(li);
+    
+    li.innerText = perItem;
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];

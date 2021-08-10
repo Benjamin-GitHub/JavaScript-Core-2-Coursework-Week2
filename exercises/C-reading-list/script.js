9495a2f1de5426ec7b8f7514076ef42c2a14ce24
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-08-10 23:34:10 +0100

    Exercise C
    Completed

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,23 +1,58 @@
 function readingList(books) {
-  // Write your code here...
+  let content = document.querySelector("#content");
+
+  let h1 = document.createElement("h1");
+  content.appendChild(h1);
+  h1.innerText = "Book List";
+
+  let ul = document.createElement("ul");
+  content.appendChild(ul);
+  ul.style.display = "flex";
+  ul.style.flexWrap = "wrap";
+  ul.style.alignItems = "column";
+
+  books.forEach((bookDetails) => {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.style.padding = "30px";
+    li.style.margin = "20px";
+    li.style.justifyContent = "space-between";
+
+    let p = document.createElement("p");
+    li.appendChild(p);
+    p.innerText = `${bookDetails.title} - ${bookDetails.author}`;
+
+    let image = document.createElement("img");
+    li.appendChild(image);
+    image.src = bookDetails.bookCover;
+    image.style.maxHeight = "270px";
+    image.style.maxWidth = "180px";
+
+    if (books.alreadyReads) {
+      li.style.backgroundColor = "green";
+    } else li.style.backgroundColor = "red";
+  });
 }
 
 const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    bookCover: "./image/The-Design-of-Everyday-Things-coverbook.png",
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    bookCover: "./image/The-Most-Human-Human-coverbook.png",
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
-  }
+    alreadyRead: true,
+    bookCover: "./image/The-Pragmatic-Programmer-coverbook.png",
+  },
 ];
 
 readingList(books);

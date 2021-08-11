7aca4b020e84a7fd50bd9a8e1bf69d0953e3af80
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-08-12 00:19:38 +0100

    Exercise E list-of-colours
    Completed

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,22 @@
 function listOfColours(colours) {
-  // Write your code here...
+  let content = document.querySelector("#content");
+  let select = document.createElement("select");
+  content.appendChild(select);
+  let p = document.createElement("p");
+  content.appendChild(p);
+
+  colours.forEach((colour) => {
+    let option = document.createElement("option");
+    select.appendChild(option);
+    option.textContent = colour;
+    option.style.color = colour;
+
+    select.addEventListener("click", () => {
+      p.innerText = select.value;
+      p.style.color = select.value;
+    });
+    
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];

f655863c7eb4f8bd3f0ea1a4391f7ba2f4919a74
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-08-11 22:50:00 +0100

    Exercise D "todo-list"
    Completed

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,31 @@
 function todoList(todos) {
-  // Write your code here...
+  let content = document.querySelector("#content");
+
+  let ul = document.createElement("ul");
+  content.appendChild(ul);
+  // ul.style.display = "flex";
+  ul.style.flexDirection = "column";
+
+  todos.forEach((items) => {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.style.display = "flex";
+    li.style.padding = "10px";
+    li.innerText = items.todo;
+
+    li.addEventListener("click", () => {
+      if ((li.style.textDecoration = "none")) {
+        li.style.textDecoration = "line-through";
+      }
+    });
+  });
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
